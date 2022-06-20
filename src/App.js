03c461644f1ff234cdd7bd88966d564c028b12a4
wmaKRET <wkret91.2@gmail.com>
2022-06-20 12:05:44 +0200

    added css and components layout in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Field from "./Field"
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="container">
+      <div className="header">
+        <h1>Tic Tac Toe</h1>
+        <div className="header__menu">
+          <button>Restart</button>
+          <div className="header__menu-next-mark">
+            <h2>O</h2>
+          </div>
+        </div>
+      </div>
+      <div className="game">
+        <Field />      
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Field.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Field(){
+    return (
+        <div className="game__field">
+            <h2>X</h2>
+        </div>
+    )
+}
